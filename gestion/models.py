69261e851ad26ba95ad6ee56dee590caf97c9cb2
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2024-10-24 15:40:41 +0200

    create models and migrate

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,52 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        name = f'{self.firstname} {self.lastname}'
+        return name
+
+    class Meta:
+        ordering = ['lastname', 'firstname']
+        unique_together = ('firstname', 'lastname')
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ('name', 'creator')
+
+
+class Loan(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    loan_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.member} {self.item} {self.loan_date}'
+
+    class Meta:
+        unique_together = ('member', 'item', 'loan_date')
